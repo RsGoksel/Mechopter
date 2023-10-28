ea1a33510384cfaf2a8cdb7cceee87016b7791f8
Göksel Gündüz <80707238+RsGoksel@users.noreply.github.com>
2023-10-28 20:58:10 +0300

    Readability has ben improved

--- Agent.py
+++ Agent.py
@@ -5,17 +5,13 @@ import os
 # Screen
 WIDTH, HEIGHT =  Constants.WIDTH, Constants.HEIGHT
 
-
 class Drone(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        
         self.reset()
         
-        
     def reset(self):
-        
-        # Reset variables
+    # Reset initial variables
         (self.angle, self.angle_speed, self.angular_acceleration) = (0, 0, 0)
-        (self.x_position, self.x_speed, self.x_acceleration)      = (int(WIDTH/2) , 0, 0)
-        (self.y_position, self.y_speed, self.y_acceleration)      = (int(HEIGHT/2), 0, 0)
+        (self.x_position, self.x_speed, self.x_acceleration) = (int(WIDTH/2) , 0, 0)
+        (self.y_position, self.y_speed, self.y_acceleration) = (int(HEIGHT/2), 0, 0)
